8e8b12151f7723ca213e4dbffc349200bacecd5a
Artem Shadrunov <artemshadrunov@gmail.com>
2020-10-03 18:40:11 +0300

    Upd: client added

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,7 @@ namespace API
         {
             services.AddDbContext<DataContext>(options => options.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
